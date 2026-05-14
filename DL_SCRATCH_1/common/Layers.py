641ed65c6025059ef640e6764b074f582a407ad0
2hwa <2flower4019@gmail.com>
2026-05-14 22:46:35 +0000

    CNN implementation update

--- DL_SCRATCH_1/common/Layers.py
+++ DL_SCRATCH_1/common/Layers.py
@@ -149,7 +149,46 @@ class Dropout:
         return dout * self.mask
 
 
+
+im2col = f.im2col
+col2im = f.col2im
+
+class Convolution:
+    def __init__ (self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+        # 역전파 시 필요한 중간 데이터
+        self.x = None
+        self.col = None
+        self.col_W = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        return out
+        
+        
+
+
     
+
+
+
 
 #test code
 #swl = softmaxwithloss()
